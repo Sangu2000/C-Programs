2c01311dc52af514635de10c3eb376afaeff18b1
Sangu2000 <91773856+Sangu2000@users.noreply.github.com>
2022-04-01 23:40:23 +0530

    Add files via upload
    Basic c programs

---
+++ 01_basic.c
@@ -0,0 +1,12 @@
+ #include<stdio.h>
+int main()
+{
+    printf("Hello world\n ");
+    printf("Hellow Kaustubh");
+return(0);
+}
+#include<stdio.h>
+int main(){
+    printf("Hello my Name is Sangamesh\n");
+    return(0);
+}
---
+++ 03_first.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+    printf("Hellow my name is .....");
+    return(0);
+
+}
