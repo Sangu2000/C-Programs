7c3cac5cdffe58760b45e983efd88de2ba389748
Sangu2000 <91773856+Sangu2000@users.noreply.github.com>
2022-04-02 22:48:58 +0530

    Add files via upload

---
+++ 05_pr01.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    int a ;
+    int b ;
+   printf("Hello Word");
+    printf("Enter length of rectangle\n");
+    scanf("%d", &a);
+    printf("Enter breadth of rectangle\n");
+    scanf("%d", &b);
+    printf("The area of recangle is %d", a*b);
+    return 0 ;
+}
