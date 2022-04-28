8c3c8a6f379d3107dca9903cf5cb6c3cb889564a
Sangu2000 <91773856+Sangu2000@users.noreply.github.com>
2022-04-28 22:37:49 +0530

    Add files via upload

---
+++ 01_variables.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string charName = "Sangamesh";
+    int charAge = 22;
+    cout<<" the characters name is "<< charName <<endl;
+    cout<<" and his age is "<< charAge <<endl;
+    return 0;
+}
