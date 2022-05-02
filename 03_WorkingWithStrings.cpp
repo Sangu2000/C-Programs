f5798513af5c4272a68d6b442b504d2bd8e014c2
Sangu2000 <91773856+Sangu2000@users.noreply.github.com>
2022-05-02 21:04:55 +0530

    Add files via upload

---
+++ 03_WorkingWithStrings.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // cout<<"why should we listen \n "<<endl;
+    // cout<<"Sangamesh"<<endl ;
+    string phrase = "Kaustubh";
+    // cout<<phrase;
+//  cout<<phrase.length();
+//  phrase[0] ='G';
+//  cout<<phrase ;
+// cout<<phrase.find("bh",0);
+// cout<<phrase.substr(3,4);
+string subphrase=phrase.substr(3,4);
+ cout<<subphrase;
+    return 0;
+}
---
+++ 04_WorkingWithNumbers.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    int num = 6;
+    double dnum = 5.5328;
+    dnum++;
+    num+=20;
+    cout<<num;
+    cout<<pow(2,6);
+    // cout<<sqrt(6);
+    // cout<<round(6.3);
+    // cout<<ceil(4.1);
+    // cout<<floor(5.8);
+    // cout<<fmax(2,10);
+    return 0;
+}
