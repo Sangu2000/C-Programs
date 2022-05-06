1537991a89dc5eb6f9c1fad9df70e1bd33017b56
Sangu2000 <91773856+Sangu2000@users.noreply.github.com>
2022-05-06 22:27:23 +0530

    Add files via upload

--- 09_Function.cpp
+++ 09_Function.cpp
@@ -1,5 +1,12 @@
 #include<iostream>
 #include<cmath>
+using namespace std;
+void sayHii(string name,float marks){
+    cout<<"hello"<< name << " your marks are "<<marks <<endl;
+}
 int main(){
+    sayHii(" Sangamesh",95.05);
+    sayHii(" Sanjay",93.05);
+    sayHii(" Swarali",98.05);
     return 0;
 }
