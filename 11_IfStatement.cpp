685e49f75c33cadd01967d8dc17af93fd98d9616
Sangu2000 <91773856+Sangu2000@users.noreply.github.com>
2022-05-10 21:46:14 +0530

    Add files via upload

---
+++ 11_IfStatement.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main(){
+// bool isMale = true;
+// bool Tall = true;
+// if (isMale && Tall)
+// {
+//     cout<<"You are  a tall Male ";
+   
+// }
+// else
+// {
+//  cout<<"you are not Male as well as you are not tall";
+// }
+bool isMale = true;
+bool Tall =false;
+if (isMale || Tall)
+{
+   cout<<"You are a good Male";
+}
+else{
+cout << "You are not a good Male";
+}
+
+    return 0;
+}
